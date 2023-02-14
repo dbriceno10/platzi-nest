31d657f75871ae32b22ae2655b8013e2b6577e16
Daniel Briceno <daniel.b100726@gmail.com>
2023-02-14 16:59:55 -0400

    Class 11: Qué es el método POST

--- src/controllers/categories/categories.controller.ts
+++ src/controllers/categories/categories.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get, Param } from '@nestjs/common';
 export class CategoriesController {
   @Get(':id/:producs/:productId')
   getCategory(@Param('id') id: string, @Param('productId') productId: string) {
-    return `category ${id} product ${productId}`;
+    return { message: `category ${id} product ${productId}` };
   }
 }
--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -1,16 +1,16 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query, Post, Body } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
   //para evitar choques de rutas, debemos hacer que todas las rutas que no sean dinamicas vayan de primeras...
   @Get('filter')
   getProductFilter() {
-    return `soy un filtro`;
+    return { message: `soy un filtro` };
   }
 
   @Get(':id')
   getProduct(@Param('id') id: string) {
-    return `product ${id}`;
+    return { message: `product ${id}` };
   }
 
   @Get('')
@@ -19,6 +19,27 @@ export class ProductsController {
     @Query('offset') offset = 0, //si le pasamos un parametro por defecto, usamos tipado inferido
     @Query('brand') brand: string,
   ) {
-    return `products limit: ${limit} offset: ${offset} brand: ${brand}`;
+    return {
+      message: `products limit: ${limit} offset: ${offset} brand: ${brand}`,
+    };
   }
+
+  @Post()
+  create(@Body() payload: any) {
+    return {
+      message: 'Accion para crear',
+      payload
+    };
+  }
+
+  /* @Post()
+  create(@Body('name') name: string, @Body('price') price: number) {
+    return {
+      message: 'Accion para crear',
+      payload: {
+        name,
+        price,
+      },
+    };
+  } */
 }
