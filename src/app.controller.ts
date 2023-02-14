6dbb381ec2cd241cf48af047918b9e2cad2d9862
Daniel Briceno <daniel.b100726@gmail.com>
2023-02-14 15:56:33 -0400

    Class 7: GET: cómo recibir parámetros

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,18 +8,28 @@ export class AppController {
   @Get()
   getHello(): string {
     // return this.appService.getHello();
-    return 'Hola mundo'
+    return 'Hola mundo';
   }
 
   //manejo de rutas simples, no nos debemos preocupar por poner el '/'
 
   @Get('new')
   newEndpoint() {
-    return 'I`m new endpoint'
+    return 'I`m new endpoint';
   }
 
   @Get('hello/world')
   hello() {
-    return 'Hello Moto!'
+    return 'Hello Moto!';
+  }
+
+  @Get('products/:id')
+  getProduct(@Param('id') id: string) {
+    return `product ${id}`;
+  }
+
+  @Get('categories/:id/:producs/:productId')
+  getCategory(@Param('id') id: string, @Param('productId') productId: string) {
+    return `category ${id} product ${productId}`;
   }
 }
