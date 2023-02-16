af7432c0fd2e410628c78b5d220317230cac2ca9
Daniel Briceno <daniel.b100726@gmail.com>
2023-02-16 12:02:42 -0400

    Class 21: Cómo evitar parámetros incorrectos

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  //Activamos las validaciones...
-  app.useGlobalPipes(new ValidationPipe())
+  //*Activamos las validaciones...
+  app.useGlobalPipes(
+    new ValidationPipe({
+      //*Esta opcion lo que va a hacer es quitar de lo que se envie ne el cuerpo de un request todo lo que no cumpla con el formato del DTO
+      whitelist: true,
+      //*Si enviamos atributos en el body del request que no son validos, va a lanzar un error
+      forbidNonWhitelisted: true
+    }),
+  );
   await app.listen(3200);
 }
 bootstrap();
